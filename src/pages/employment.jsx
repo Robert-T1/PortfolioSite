2d807b2fcf2b447b3b19b7a63110c9de2d26ee0a
Robert-T1 <110181074+Robert-T1@users.noreply.github.com>
2025-09-06 11:04:38 -0500

    Update employment.jsx

--- src/pages/employment.jsx
+++ src/pages/employment.jsx
@@ -11,8 +11,8 @@ function Employment() {
 
                 <EmploymentElement description="Founded and operated an independent game studio to build commercial games and grow as a software engineer. Led all major projects and took on every role—gameplay engineer, UI engineer, designer, artist, marketer, and publisher. Released three games with hundreds of downloads and thousands of plays across platforms." 
                 label="RELT Games - Game Developer" tenure="April 2021 - Now" employmentType="Full-Time"/>
-                <EmploymentElement description="Work as a cashier handling customer checkout and service, with a strong track record of punctuality and reliability. Gain experience with both customer-facing and internal software systems. Recognized as Front End Team Member of the Month for performance."
-                 label="Lowe's - Cashier" tenure="Novemeber 2024 - Now" employmentType="Part-Time"/>
+                <EmploymentElement description="Work as a retail associate supporting customer service, sales, and daily store operations, with a strong track record of punctuality and reliability. Gain experience using both customer-facing and internal store systems. Recognized as Front End Team Member of the Month for performance.."
+                 label="Lowe's - Retail Associate" tenure="Novemeber 2024 - Now" employmentType="Part-Time"/>
                 <EmploymentElement description="Supported the care and treatment of animals in a clinical setting. Assisted with procedures, administered medications, performed cleaning and maintenance tasks, and provided general care for dogs and cats."
                  label="Pet Clinic Of Neosho - Vet Technician" tenure="April 2020 - October 2023" employmentType="Part-Time"/>
                 <EmploymentElement description="Contributed to the production and sale of fresh produce and handmade goods at local seasonal farmers markets. Managed customer service, cash handling, garden maintenance, and offered knowledgeable support on various products."
